a60bc35aa46c5dc9e5bfe108f1b8b71e3130dae0
mUniKeS <munikes@members.fsf.org>
2018-07-24 14:07:38 +0200

    Script view all events

--- scripts/view_event.js
+++ scripts/view_event.js
@@ -4,17 +4,20 @@ require('dotenv').config();
 
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.ethichub.com'));
-console.log('remote: ' + web3.version.node);
-var BN = web3.utils.BN;
+//console.log('remote: ' + web3.version);
+console.log('remote: ' + web3.version);
+//var BN = web3.utils.BN;
 const loader = require('./contract_loader.js');
 
 var contractAddress = '0xF86BEbaB4894012de35925eB57a6b11666f32704';
 
 loader.load(web3, 'EthicHubLending',contractAddress).then( async (contractInstance) =>  {
-
-    var event_filter = web3.eth.filter({"address": contractAddress})
-    console.log(event_filter);
-
-    return '';
+    contractInstance.getPastEvents('allEvents', {
+            fromBlock: 0,
+            toBlock: 'latest'
+    }, function(error, events){ console.log(events); })
+    .then(function(events){
+            console.log(events) // same results as the optional callback above
+    });
 
 });
