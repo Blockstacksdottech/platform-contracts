8bc20ad591f6de5eec45324574b8e703c1199d0a
mUniKeS <munikes@members.fsf.org>
2019-01-24 15:33:32 +0100

    WIP test single case

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,6 +7,7 @@ const storage = artifacts.require('./storage/EthicHubStorage.sol');
 const cmc = artifacts.require('./EthicHubCMC.sol');
 const reputation = artifacts.require('./reputation/EthicHubReputation.sol');
 const userManager = artifacts.require('./user/EthicHubUser.sol');
+const arbitrage = artifacts.require('./arbitrage/EthicHubArbitrage.sol');
 
 // Deploy EthicHub network
 module.exports = async (deployer, network) => {
@@ -21,12 +22,12 @@ module.exports = async (deployer, network) => {
         console.log("--> Deploying EthichubCMC...");
         return deployer.deploy(cmc, storage.address).then(() => {
             console.log("--> EthichubCMC deployed");
-
             return storage.deployed().then(async storageInstance => {
                 //Give CMC access to storage
                 console.log("--> Registering EthichubCMC in the network...");
                 await storageInstance.setAddress(utils.soliditySha3("contract.address", cmc.address), cmc.address);
                 console.log("--> EthichubCMC registered");
+
                 //Deploy reputation
                 console.log("--> Deploying EthicHubReputation...");
                 return deployer.deploy(reputation, storage.address).then(() => {
@@ -36,14 +37,29 @@ module.exports = async (deployer, network) => {
                         console.log("--> Registering EthicHubReputation in the network...");
                         await cmcInstance.upgradeContract(reputation.address,"reputation");
                         console.log("--> EthicHubReputation registered");
+
+                        //Deploy users
                         console.log("--> Deploying EthicHubUser...");
                         return deployer.deploy(userManager,storage.address).then(() => {
                             console.log("--> EthicHubUser deployed");
-                            console.log("--> Registering EthicHubReputation in the network...");
+                            //Set deployed user's role in the network
                             return cmc.deployed().then(async cmcInstance => {
+                                console.log("--> Registering EthicHubUser in the network...");
                                 await cmcInstance.upgradeContract(userManager.address,"users");
                                 console.log("--> EthicHubReputation registered");
-                                console.log("--> EthicHub network ready");
+
+                                //Deploy arbitrage
+                                console.log("--> Deploying EthicHubArbitrage...");
+                                return deployer.deploy(arbitrage,storage.address).then(() => {
+                                    console.log("--> EthicHubArbitrage deployed");
+                                    //Set deployed user's role in the network
+                                    return cmc.deployed().then(async cmcInstance => {
+                                        console.log("--> Registering EthicHubArbitrage in the network...");
+                                        await cmcInstance.upgradeContract(arbitrage.address,"arbitrage");
+                                        console.log("--> EthicHubArbitrage registered");
+                                        console.log("--> EthicHub network ready");
+                                    });
+                                });
                             });
                         });
                     });
