f2638d3a73563d93405d769c3418cde5363c56c3
Iván José Martín García <ivanmartinga@gmail.com>
2019-11-13 18:22:28 +0100

    Remove logger event

--- contracts/lending/EthicHubLending.sol
+++ contracts/lending/EthicHubLending.sol
@@ -75,8 +75,6 @@ contract EthicHubLending is EthicHubBase, Pausable, Ownable {
     event onReturnAmount(address indexed borrower, uint amount);
     event onBorrowerChanged(address indexed newBorrower);
     event onInvestorChanged(address indexed oldInvestor, address indexed newInvestor);
-    // FIXME remove this
-    event Log(uint thing);
 
     // Modifiers
     modifier checkProfileRegistered(string memory profile) {
@@ -388,8 +386,6 @@ contract EthicHubLending is EthicHubBase, Pausable, Ownable {
     function contributeWithAddress(address contributor, uint256 amount) internal whenNotPaused {
         require(state == LendingState.AcceptingContributions, "state is not AcceptingContributions");
         require(isContribPeriodRunning(), "can't contribute outside contribution period");
-        
-        emit Log(1);
 
         uint oldTotalContributed = totalContributed;
         uint newTotalContributed = 0;
